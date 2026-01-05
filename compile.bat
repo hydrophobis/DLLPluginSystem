091945ec9cc7a9fcb57aa7c1f5a7ddf9298d4a18
hydrophobis <142858559+hydrophobis@users.noreply.github.com>
2026-01-05 08:25:48 -0600

    Delete compile.bat

--- compile.bat
+++
@@ -1,8 +0,0 @@
-cd plugins
-clang++.exe -shared -o console.dll .\console.cc
-clang++.exe -shared -o heartbeat.dll .\heartbeat.cc
-clang++.exe -shared -o logger.dll .\logger.cc
-clang++.exe -shared -o python.dll .\python.cc ..\ini.cc -I"C:\Users\Kasen\AppData\Local\Programs\Python\Python314\include" -L"C:\Users\Kasen\AppData\Local\Programs\Python\Python314\libs" -lpython314
-
-cd ..
-clang++.exe -o runtime .\runtime.cc .\ini.cc
